adbdc506ab688726a9f0772e16d2720b4b466af9
Nizamph <116118003+Nizamph@users.noreply.github.com>
2024-01-14 13:47:18 +0530

    command added for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,31 @@ const userRoutes = require('./routes/authRoutes');
 const employeeRoutes = require('./routes/employeeRoutes');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
 dotenv.config();
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('api is running successfully');
-});
-
 app.use('/api/auth', userRoutes);
 app.use('/api/employee', employeeRoutes);
 
-const PORT = process.env.PORT || 4000;
+// -----------------Deployment------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname1, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('api is running successfully');
+  });
+}
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`server is started on PORT ${PORT}`);
+  console.log(`server is running on PORT ${PORT}`);
 });
